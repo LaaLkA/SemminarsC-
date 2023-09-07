9493f45c29a320f18dd7310702838d20226783d2
Nikita <Wekly1@gmail.com>
2023-09-07 17:27:59 +0300

    Completed Task8

--- Homework#1/Task#8/Program.cs
+++ Homework#1/Task#8/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+Console.WriteLine("Enter a number:  ");
+string strNumber = Console.ReadLine();
+int Number = int.Parse(strNumber);
+
+int massNumber = -Number;
+
+while (massNumber <= Number)
+{
+    if (massNumber % 2 == 0)
+    {
+        Console.Write(massNumber);
+        Console.Write(", ");
+        massNumber++; 
+    }
+    else
+    {
+        massNumber++;
+    } 
+} 
