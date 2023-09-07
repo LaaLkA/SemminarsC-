8b7a86cb5c0dad63f35245a79228020ff5bfce8d
Nikita <Wekly1@gmail.com>
2023-09-07 11:49:06 +0300

    Wrote Task

--- Sem#1/Program.cs
+++ Sem#1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное само на себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
