b46ef22905219b5231eca10f24e347203c802a18
Nikita <Wekly1@gmail.com>
2023-09-07 17:23:36 +0300

    Completed Task6

--- Homework#1/Task#6/Program.cs
+++ Homework#1/Task#6/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.Clear();
+
+Console.WriteLine("Enter a number:  ");
+string strNumber = Console.ReadLine();
+int Number = int.Parse(strNumber);
+
+if (Number % 2 == 0)
+{
+    Console.WriteLine("Число чётное!");
+}
+else 
+{
+    Console.WriteLine("Число нечётное!");
+}
