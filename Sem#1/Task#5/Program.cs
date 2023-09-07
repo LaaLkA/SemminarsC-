e816e410e54a1d67be0d57fe7f6e9ec9344d7f46
Nikita <Wekly1@gmail.com>
2023-09-07 16:54:20 +0300

    Comleted Task5

--- Sem#1/Task#5/Program.cs
+++ Sem#1/Task#5/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание №6. Напишите программу, в которую будет заводиться трёхзначное число, а программа на выходе будет давать последнюю (третью) цифру.
+// 324 -> 4
+// 111 -> 1
+
+Console.Clear();
+
+Console.WriteLine("Enter three digit number:  ");
+string strNumber = Console.ReadLine();
+int Number = int.Parse(strNumber);
+
+if (Number > 99)
+{
+    Console.WriteLine(Number % 10);
+}
+else
+{
+    Console.WriteLine("Введите число большее 99");
+}
