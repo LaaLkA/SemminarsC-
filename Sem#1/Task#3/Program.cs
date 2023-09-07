7e09b0055c8851c817f337be5f1358f1541f160c
Nikita <Wekly1@gmail.com>
2023-09-07 14:02:08 +0300

    Updated the solution with the semminar

--- Sem#1/Task#3/Program.cs
+++ Sem#1/Task#3/Program.cs
@@ -12,31 +12,31 @@ if (NumberDay == 1)
 {
 Console.WriteLine("Это Понедельник!");
 }
-if (NumberDay == 2)
+else if (NumberDay == 2)
 {
 Console.WriteLine("Это Вторник!");
 }
-if (NumberDay == 3)
+else if (NumberDay == 3)
 {
 Console.WriteLine("Это Среда!");
 }
-if (NumberDay == 4)
+else if (NumberDay == 4)
 {
 Console.WriteLine("Это Четверг!");
 }
-if (NumberDay == 5)
+else if (NumberDay == 5)
 {
 Console.WriteLine("Это Пятница!");
 }
-if (NumberDay == 6)
+else if (NumberDay == 6)
 {
 Console.WriteLine("Это Суббота!");
 }
-if (NumberDay == 7)
+else if (NumberDay == 7)
 {
 Console.WriteLine("Это Воскресенье!");
 }
-if (NumberDay > 7)
+else 
 {
 Console.WriteLine("В недели только 7 дней, напишите число от 1 до 7 включительно");
 }
