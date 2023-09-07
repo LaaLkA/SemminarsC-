9dc7afdb877845ad56cfca95edce1e0eaf21d7e1
Nikita <Wekly1@gmail.com>
2023-09-07 14:21:45 +0300

    Completed Task4

--- Sem#1/Task#4/Program.cs
+++ Sem#1/Task#4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача№5. Напишите программу, которая на вход принимает одно число (N), а на вызоде показывает целые числа в промежутке [-N, N]
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 1 -> "-1, 0, 1"
+
+Console.Clear();
+
+Console.WriteLine("Enter number:   ");
+string StrNumber = Console.ReadLine();
+int Number = int.Parse(StrNumber);
+
+int massNumber = -Number;
+
+while (massNumber <= Number)
+{
+    Console.Write(massNumber);
+    Console.Write(", ");
+    massNumber++;
+} 
+
+ 
