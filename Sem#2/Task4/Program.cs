cbafa5c2a4eeda48f2fec6cf5447188c97b05f41
Nikita <Wekly1@gmail.com>
2023-09-11 15:04:26 +0300

    Completed Task4

--- Sem#2/Task4/Program.cs
+++ Sem#2/Task4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и проверяет кратно ли оно одновременно 7 и 23
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число");
+string strNumber = Console.ReadLine();
+int Number = int.Parse(strNumber);
+
+int del1 = 7;
+int del2 = 23;
+
+if (Number % del1 == 0 && Number % del2 == 0)
+{
+    System.Console.WriteLine($"Число {Number} кратно {del1} и {del2}");
+}
+else
+{
+    System.Console.WriteLine($"Число {Number} не кратно {del1} и {del2}");
+}
