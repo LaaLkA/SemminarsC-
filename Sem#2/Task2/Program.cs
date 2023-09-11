7f869107c5b7816af385875baef1d4cff2cf81e7
Nikita <Wekly1@gmail.com>
2023-09-11 14:38:03 +0300

    Дополнено: Completed task2

--- Sem#2/Task2/Program.cs
+++ Sem#2/Task2/Program.cs
@@ -7,7 +7,6 @@ int Number = new Random().Next(100,1000);
 Console.WriteLine($"Полученное случайное трехзначное число: {Number}");
 
 int Number1 = Number / 100;
-int Number2 = (Number - Number1*100) / 10;
-int Number3 = (Number - Number1*100) % 10;
+int Number3 = Number % 10;
 
 Console.WriteLine($"Число после удаления второй цифры: {Number1}{Number3}");
