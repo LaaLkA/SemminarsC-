0126ae84d675625bddbfc95d8796ef1a36549cfa
Nikita <Wekly1@gmail.com>
2023-09-11 15:52:12 +0300

    Completed task6

--- Sem#2/Task6/Program.cs
+++ Sem#2/Task6/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Clear();
+
+System.Console.WriteLine("Введите трёхзначное число");
+string strNumber = Console.ReadLine();
+int Number = int.Parse(strNumber);
+
+if (Number > 99 && Number < 1000)
+{
+    int secondNumber = Number %100 / 10;
+    System.Console.WriteLine($"вторая цифра заданного числа = {secondNumber}");
+}
+else
+{
+    System.Console.WriteLine("Вы ввели некорректное число!");
+}
