1bde1560e66908bc91e5b110affd06585be19240
Nikita <Wekly1@gmail.com>
2023-09-11 15:14:12 +0300

    Completed Task5

--- Sem#2/Task5/Program.cs
+++ Sem#2/Task5/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход два числа и проверяет является ли одно число квадратом другого и наоборот.
+
+Console.Clear();
+
+System.Console.WriteLine("Введите первое число");
+string strNumber1 = Console.ReadLine();
+int Number1 = int.Parse(strNumber1);
+
+System.Console.WriteLine("Введите второе число");
+string strNumber2 = Console.ReadLine();
+int Number2 = int.Parse(strNumber2);
+
+if (Number1 * Number1 == Number2 || Number2 * Number2 == Number1) // Используем оператор или "||"
+{
+   System.Console.WriteLine("Одно из чисел является квадратом другого"); 
+}
+else 
+{
+    System.Console.WriteLine("Ни одно из чисел не является квадратом другого");
+}
