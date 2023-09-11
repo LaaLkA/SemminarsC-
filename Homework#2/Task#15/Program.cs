788e3f3871ec4535ddf88a78b457bca2ae86b312
Nikita <Wekly1@gmail.com>
2023-09-11 16:46:42 +0300

    Created a new folder TASK15

--- Homework#2/Task#15/Program.cs
+++ Homework#2/Task#15/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+
+System.Console.WriteLine("Введите цифру, обозначающую день недели в РФ");
+string strNumber = Console.ReadLine();\
+int Number = int.Parse(strNumber);
+
+if (Number < 7 && Number > 0)
+{
+    if (Number > 5)
+    {
+        System.Console.WriteLine($"{Number} день недели соответствует выходному дню");
+    }
+    else 
+    {
+        System.Console.WriteLine($"{Number} день недели соответствует рабочему дню");
+    }
+}
+else 
+{
+    System.Console.WriteLine($"Цифра {Number} некорректная");
+}
