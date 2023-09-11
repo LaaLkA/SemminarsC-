8d20415d06b961a45992beaf7ebddd539ca17289
Nikita <Wekly1@gmail.com>
2023-09-11 14:33:21 +0300

    Completed Task2

--- Sem#2/Task2/Program.cs
+++ Sem#2/Task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное трехзначное число и удляет вторую цифру этого числа
+// 456 -> 46
+
+Console.Clear();
+
+int Number = new Random().Next(100,1000);
+Console.WriteLine($"Полученное случайное трехзначное число: {Number}");
+
+int Number1 = Number / 100;
+int Number2 = (Number - Number1*100) / 10;
+int Number3 = (Number - Number1*100) % 10;
+
+Console.WriteLine($"Число после удаления второй цифры: {Number1}{Number3}");
