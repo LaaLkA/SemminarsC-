6e7f5ae207dbb410776f263b298015b1dada6b74
Nikita <Wekly1@gmail.com>
2023-09-12 17:46:49 +0300

    Completed Task2

--- Sem#3/Task2/Program.cs
+++ Sem#3/Task2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Clear();
+
+int TakeNumber(string message)
+{
+    Console.Write($"Введите {message}: ");
+    int Number = Convert.ToInt32(Console.ReadLine());
+    if(Number < 0) Console.WriteLine("Номер четверти не может быть отрицательным!");
+    else if(Number > 4) Console.WriteLine("Существует всего 4 четверти!");
+    return Number;
+}
+
+void ShowDiap (int Number)
+{
+    if (Number == 1) Console.WriteLine($"{Number} четверти соответствуют значения: X > 0, y > 0");
+    else if(Number == 2) Console.WriteLine($"{Number} четверти соответствуют значения: X < 0, y > 0");
+    else if(Number == 3) Console.WriteLine($"{Number} четверти соответствуют значения: X < 0, y < 0");
+    else if(Number == 4) Console.WriteLine($"{Number} четверти соответствуют значения: X > 0, y < 0");
+}
+
+int Number = TakeNumber("Четверть");
+ShowDiap(Number);
