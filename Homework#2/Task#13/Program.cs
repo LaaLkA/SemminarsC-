53aa3d61559cf5717a3731e0dbf65bc1e23d10d9
Nikita <Wekly1@gmail.com>
2023-09-12 10:06:58 +0300

    just a commit

--- Homework#2/Task#13/Program.cs
+++ Homework#2/Task#13/Program.cs
@@ -14,33 +14,33 @@ void FindThird(int Arg)
     int result = 0;
 
     if (Arg < 100)
-        {
-            System.Console.WriteLine("Нет третьей цифры");
-        }
-        else if (Arg < 1000)
-        {
-            result = Arg % 10;
-            System.Console.WriteLine($"Третья цифра числа {Arg} равна {result}");
-        }
-        else if (Arg < 10000)
-        {
-            result = Arg % 100 / 10;
-            System.Console.WriteLine($"Третья цифра числа {Arg} равна {result}");
-        }
-         else if (Arg < 100000)
-        {
-            result = Arg % 1000 / 100;
-            System.Console.WriteLine($"Третья цифра числа {Arg} равна {result}");
-        }
-          else if (Arg < 1000000)
-        {
-            result = Arg % 10000 / 1000;
-            System.Console.WriteLine($"Третья цифра числа {Arg} равна {result}");
-        }
-        else 
-        {
-            System.Console.WriteLine("Слишком большое число");
-        }
+    {
+        System.Console.WriteLine("Нет третьей цифры");
+    }
+    else if (Arg < 1000)
+    {
+        result = Arg % 10;
+        System.Console.WriteLine($"Третья цифра числа {Arg} равна {result}");
+    }
+    else if (Arg < 10000)
+    {
+        result = Arg % 100 / 10;
+        System.Console.WriteLine($"Третья цифра числа {Arg} равна {result}");
+    }
+    else if (Arg < 100000)
+    {
+        result = Arg % 1000 / 100;
+        System.Console.WriteLine($"Третья цифра числа {Arg} равна {result}");
+    }
+    else if (Arg < 1000000)
+    {
+        result = Arg % 10000 / 1000;
+        System.Console.WriteLine($"Третья цифра числа {Arg} равна {result}");
+    }
+    else
+    {
+        System.Console.WriteLine("Слишком большое число");
+    }
 }
 
 FindThird(Number);
