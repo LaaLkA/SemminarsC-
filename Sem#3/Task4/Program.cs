d761be7fbd090a71aa24bde3016ace1cb3711c5b
Nikita <Wekly1@gmail.com>
2023-09-13 17:01:32 +0300

    Completed task4

--- Sem#3/Task4/Program.cs
+++ Sem#3/Task4/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+// Например:
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Clear();
+int takeNumber(string message)
+{
+    Console.WriteLine($"Введите {message}");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void tableUmn(int number)
+{
+    for (int i = 0; i <= number; i++)
+    {
+        int res = number * i;
+        Console.WriteLine($"{number} * {i} = {res}"); 
+    }
+}
+int number = takeNumber("число");
+tableUmn(number);
