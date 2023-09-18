a860f472d1af2f26e434e08fbb04ee5240eb227f
Nikita <Wekly1@gmail.com>
2023-09-18 10:32:33 +0300

    Completed a task1

---
+++ Sem#4/Task#1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+
+int GetNumber (string message)
+{
+    Console.WriteLine($"Enter {message}");
+    int Number = Convert.ToInt32(Console.ReadLine());
+    return Number;
+}
+int GetSum (int Number)
+{
+    int Sum = 0;
+    for (int i = 0; i <= Number; i++)
+    {
+        Sum += i;
+    }
+    return Sum;
+}
+void Write (int Number, int Sum)
+{
+    Console.WriteLine($"{Number} -> {Sum}");
+}
+int Number = GetNumber("number");
+int Sum = GetSum(Number);
+Write(Number, Sum);
