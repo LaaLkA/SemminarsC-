787c157d000a18a52966ea654ece2941059dcc15
Nikita <Wekly1@gmail.com>
2023-09-18 14:49:50 +0300

    Completed a task27

---
+++ Homework#4/Task#27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int take()
+{
+    Console.WriteLine($"Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int Sum(int number)
+{
+   int Summ = 0;
+    for ( ; 0 < number; number /= 10 )
+    {
+        Summ = Summ + number%10;   
+    }
+    return Summ;
+}
+System.Console.WriteLine(Sum(take()));
