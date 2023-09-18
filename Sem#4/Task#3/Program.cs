cc064070d7551abf43a81212b290d3d2527567ca
Nikita <Wekly1@gmail.com>
2023-09-18 11:07:49 +0300

    Completed a task3

---
+++ Sem#4/Task#3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Hапишите программу, которая на вход принимает число N и выдаёт произведение от 1 до N.
+// 5 -> 120
+// 4 -> 24
+
+int GetNumber(string message)
+{
+    Console.WriteLine($"Enter {message}");
+    int Number = Convert.ToInt32(Console.ReadLine());
+    return Number;
+}
+int UmnNumber(int Number)
+{
+    int result = 1;
+    for (int i = 1; i <= Number; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+Console.Clear();
+int Number = GetNumber("number");
+int res = UmnNumber(Number);
+Console.WriteLine($"Произведение чисел от 1 до {Number} = {res}");
