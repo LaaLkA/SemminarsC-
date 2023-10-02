f380f3051f7037efaa9b85be4430a6192542c260
LaaLkA <you@example.com>
2023-10-02 15:10:05 +0300

    Completed a task 53

---
+++ Sem#8/Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 53: Задайте двумерный массив. 
+// Напишите программу, которая поменяет местами первую и последнюю строку массива.
+int TakeNumbers(string message)
+{
+    System.Console.WriteLine($"Enter {message}");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int[,] TakeMatrix(int rows, int columns)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+void SwitchRows(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        temp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = temp;
+    }
+}
+void Print(int[,] matrix)
+{
+    System.Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+Console.Clear();
+int[,] matrix = TakeMatrix(TakeNumbers("rows"),TakeNumbers("columns"));
+Print(matrix);
+SwitchRows(matrix);
+Print(matrix);
