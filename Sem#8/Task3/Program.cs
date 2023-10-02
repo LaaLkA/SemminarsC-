3b7ea5ccf8e473cd8a6733b62fe943a9fb80968b
LaaLkA <you@example.com>
2023-10-02 17:54:47 +0300

    Comleted a task 59

---
+++ Sem#8/Task3/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел. 
+// Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+
+int TakeNumbers(string message)
+{
+    System.Console.WriteLine($"Enter {message}");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int[,] TakeMatrix(int rows, int columns)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0, 100);
+        }
+    }
+    return matrix;
+}
+void SearchMin(int[,] matrix, out int imin, out int jmin)
+{
+    imin = 0;
+    jmin = 0;
+    int min = matrix[0, 0];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (min > matrix[i, j])
+            {
+                min = matrix[i, j];
+                imin = i;
+                jmin = j;
+            }
+        }
+    }
+}
+void Print(int[,] matrix)
+{
+    System.Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+int[,] DeleteRowCol(int[,] matrix, int row, int col)
+{
+    int[,] newMatrix = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == row || j == col) continue;
+            else 
+            {
+                newMatrix[i,j]= matrix[i,j];
+            }
+        }
+    }
+    return newMatrix;
+}
+
+Console.Clear();
+int imin;
+int jmin;
+int[,] matrix = TakeMatrix(TakeNumbers("rows"), TakeNumbers("columns"));
+Print(matrix);
+SearchMin(matrix, out imin, out jmin);
+System.Console.WriteLine($"{imin},  {jmin}");
+int[,] result = DeleteRowCol(matrix, imin, jmin);
+Print(result);
